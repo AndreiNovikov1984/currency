4fde05b14400aaa8406f9ab564ef045ddf91c227
Andrei Novikov <scanyv@yandex.ru>
2024-01-21 21:29:07 +0300

    Add openApi

---
+++ general/src/main/java/com/example/currency/model/Request.java
@@ -0,0 +1,10 @@
+package com.example.currency.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Request {
+    private final String date;
+}
